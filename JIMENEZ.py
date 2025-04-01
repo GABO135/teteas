7348bb45b6a452a17bef5179ac2a4ce6461f52bb
GABO135 <gj589056@gmail.com>
2025-04-01 12:54:35 -0500

    Update JIMENEZ.py

--- JIMENEZ.py
+++ JIMENEZ.py
@@ -1,27 +1,23 @@
-from sympy import symbols, Eq, solve
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
 def balance_combustion(formula):
-    # Elementos químicos involucrados en la combustión
-    C, H, O = symbols('C H O')
-    a, b, c, d = symbols('a b c d')
-    
     # Extraer el número de átomos de C, H y O
     carbon = int(''.join(filter(str.isdigit, formula.split('H')[0][1:]))) if 'H' in formula else 1
-    hydrogen = int(''.join(filter(str.isdigit, formula.split('O')[0].split('H')[1]))) if 'O' in formula else 2
-    oxygen = int(''.join(filter(str.isdigit, formula.split('O')[1]))) if 'O' in formula and len(formula.split('O')) > 1 else 0
+    hydrogen = int(''.join(filter(str.isdigit, formula.split('O')[0].split('H')[1]))) if 'O' in formula and len(formula.split('O')) > 1 else 2
     
-    # Definir ecuaciones de conservación de masa
-    eq1 = Eq(carbon * a, c)  # Carbono
-    eq2 = Eq(hydrogen * a, 2 * d)  # Hidrógeno
-    eq3 = Eq(2 * b, 2 * c + d)  # Oxígeno
+    # Definir coeficientes
+    a = 1  # Hidrocarburo
+    c = carbon  # CO2
+    d = hydrogen // 2  # H2O
+    b = (2 * c + d) // 2  # O2
     
-    # Resolver ecuaciones
-    solution = solve((eq1, eq2, eq3), (a, b, c, d))
+    # Encontrar el mínimo común denominador
+    factor = gcd(gcd(a, b), gcd(c, d))
     
-    if solution:
-        return f"{solution[a]} {formula} + {solution[b]} O2 -> {solution[c]} CO2 + {solution[d]} H2O"
-    else:
-        return "No se pudo balancear la ecuación."
+    return f"{a//factor} {formula} + {b//factor} O2 -> {c//factor} CO2 + {d//factor} H2O"
 
 # Ejemplo de uso
 if __name__ == "__main__":
